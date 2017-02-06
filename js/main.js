e6a7999089e7376aefc0c924618c127d7cb683ea
Ryan A. Ripley <ryanaripley@gmail.com>
2017-02-05 22:35:59 -0600

    Only call Masonry and Lightbox on the appropriate views

--- js/main.js
+++ js/main.js
@@ -1,61 +1,62 @@
 // Masonry
-
 var grid = document.querySelector('.masonry-grid');
 
-// Prevent no-javascript fallback from fading in grid too soon 
-grid.classList.toggle('fading-in');
+if (grid) {
+	// Prevent no-javascript fallback from fading in grid too soon 
+	grid.classList.toggle('fading-in');
+	var msnry; 
+	var listItems = document.querySelectorAll('.grid-item');
 
-var msnry; 
-var listItems = document.querySelectorAll('.grid-item');
+	// Remove the visible class assigned in markup
+	for (i = 0; i < listItems.length; i++) { 
+	  listItems[i].classList.toggle( 'visible' );
+	}
 
-// Remove the visible class assigned in markup
-for (i = 0; i < listItems.length; i++) { 
-  listItems[i].classList.toggle( 'visible' );
+	imagesLoaded( grid, function() {
+		var msnry = new Masonry( grid, {
+		  // options
+		  itemSelector: '.grid-item',
+		  columnWidth: '.grid-sizer',
+		  percentPosition: true
+		});
+		var i = 0;
+		// fade the grid back in
+		grid.style.opacity = "1";
+		// fade in the grid-items one at a time
+		(function fadeInImages(i) {          
+			setTimeout(function () {   
+		    listItems[i].classList.toggle( 'visible' );              
+		    if (++i < listItems.length) fadeInImages(i);
+			}, 120)
+		})(0);
+	});
 }
 
-imagesLoaded( grid, function() {
-	var msnry = new Masonry( grid, {
-	  // options
-	  itemSelector: '.grid-item',
-	  columnWidth: '.grid-sizer',
-	  percentPosition: true
-	});
-	var i = 0;
-	// fade the grid back in
-	grid.style.opacity = "1";
-	// fade in the grid-items one at a time
-	(function fadeInImages(i) {          
-		setTimeout(function () {   
-	    listItems[i].classList.toggle( 'visible' );              
-	    if (++i < listItems.length) fadeInImages(i);
-		}, 120)
-	})(0);
-});
-
-
 // Lightbox 
-var lightboxLoaded = false;
-var notMobile = function() {
-	var w = window.innerWidth
-	|| document.documentElement.clientWidth
-	|| document.body.clientWidth;
-	return w > 520;
-};
+if (document.querySelectorAll("body.gallery-layout") > 0 ) {
+	var lightboxLoaded = false;
+	var notMobile = function() {
+		var w = window.innerWidth
+		|| document.documentElement.clientWidth
+		|| document.body.clientWidth;
+		return w > 520;
+	};
 
-var loadLightbox = function() {
-	if ( notMobile() ) {
-		var lightbox = new Lightbox();
-		lightbox.load();
-		lightboxLoaded = true;
-	}
-};
-loadLightbox();
+	var loadLightbox = function() {
+		if ( notMobile() ) {
+			var lightbox = new Lightbox();
+			lightbox.load();
+			lightboxLoaded = true;
+		}
+	};
+	loadLightbox();
 
-window.addEventListener("resize", function() {
-	if ( !lightboxLoaded && notMobile() ) {
-		loadLightbox();
-	}
-});
+	window.addEventListener("resize", function() {
+		if ( !lightboxLoaded && notMobile() ) {
+			loadLightbox();
+		}
+	});
+}
 
 
 
